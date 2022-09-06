666e67d1d37bdb127f56dda8baa30c5e1330fbba
HellishNigth <kritka200@gmail.com>
2022-09-05 21:43:42 -0300

    construyendo la base

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portada</title>
+    <title>Joyas Amaju</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/theme.css')}}">
 </head>
-<body>
-    <h2>Portada del</h2>
+
+<body style="background-color:rgb(211, 211, 211)">
+    <div class="container-fluid p-0">
+        <!--usuario-->
+        <div class="row text-white m-0" style="background-color:rgba(12, 175, 175, 0.514)">
+            <div class="col-8">
+                Bienvenido <b>Señorita</b>
+            </div>
+            <div class="col-3 text-right d-none d-lg-block">
+                <small>Último inicio de sesión: 06/10/2020 a las 22:28</small>
+            </div>
+            <div class="col-1 text-right d-none d-lg-block">
+                <a href="index.html" class="text-white">Cerrar Sesión</a>
+            </div>
+        </div>
+        <!--/usuario-->
+
+        <!--navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:rgb(231, 229, 229)">
+            <a class="navbar-dark">
+                <img class="logo" src="{{asset('images/logo.png')}}">
+            </a>
+            <a class="navbar-dark" href="#">Joyas Amaju</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark" href="home.html">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="equipos.html">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Ventas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Compras</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Proveedores</a>
+                    </li>
+                    <li class="nav_item">
+                        <a class="nav-link text-dark" href="#">Ajuste de Stock</a>
+                    </li>
+                    <li class="nav_item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Configuración
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Cambiar Contraseña</a>
+                            <a class="dropdown-item" href="#">Usuario</a>
+                            <a class="dropdown-item d-lg-none" href="index.html">Cerrar Sesión</a>
+                        </div>
+                    </li>
+                    
+                </ul>
+                
+            </div>
+        </nav>
+        <!--/navbar-->
+
+        <!--contenido-->
+        
+        <!--/contenido-->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -3,10 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Sistema de Campeonato de Fútbol</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/theme.css')}}">
 </head>
-<body>
-    <h3>Inicio de sesion</h3>
+<body style="background: linear-gradient(to bottom, #026597 0%, #990099 120%);">
+    <div class="container-fluid vh-100 d-flex flex-column justify-content-lg-center">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2">
+                <div class="row bg-light shadow-lg" style="height: 25rem;">
+                    <!--titulo y logo-->
+                        <div class="col-lg-4 bg-primary d-flex flex-column justify-content-center align-items-center text-center pt-3">
+                            <div class="bg-white p-2 mb-3 rounded">
+                                <img src="{{asset('images/isotipo_usm.jpg')}}">
+                            </div>
+                            <h4 class="text-light">Sistema de Campeonato de Fútbol</h4>
+                            <h6 class="text-light">DOW020 - Diseño y Programación Orientada a la Web</h6>
+                        </div>
+                    <!--/titulo y logo-->
+
+                    <!--formulario-->
+                        <div class="col-lg-8 bg-white d-flex flex-column justify-content-center py-3">
+                            <h4>Inicio de Sesión</h4>
+                            <small>Proporcione sus credenciales para ingresar al sistema</small>
+                            <div class="card">
+                                <div class="card-body">
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="username">Nombre de Usuario:</label>
+                                            <input type="text" id="username" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password">Contraseña:</label>
+                                            <input type="password" id="password" class="form-control">
+                                        </div>
+                                        <div class="text-right">
+                                            <div class="row">
+                                                <div class="col col-lg-3 offset-lg-9">
+                                                    <a href="home.html" class="btn btn-success btn-block">Iniciar Sesión</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    <!--/formulario-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+        crossorigin="anonymous"></script>
 </body>
 </html>
